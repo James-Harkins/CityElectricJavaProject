68673c6cb79ec94314db63156c27383bc9c84ba2
James Harkins <jh.harkins@protonmail.com>
2022-07-21 17:37:09 -0600

    Feat: successfully reading ProductFile.csv, iterating through each row, and saving row data off into an array, records

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<List<String>> records = new ArrayList<>();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("ProductFile.csv"));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] values = line.split(",");
+                records.add(Arrays.asList(values));
+            }
+            System.out.println(records);
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        };
     }
 }
