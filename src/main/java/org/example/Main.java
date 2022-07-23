43929e1744929f34dac2032061d97de7122b3c00
James Harkins <jh.harkins@protonmail.com>
2022-07-22 20:13:29 -0600

    Refactor: abstract filereader and product generation into separate ProductGenerator class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,23 +1,12 @@
 package org.example;
 import com.sampleProject.Product.Proto.ProductProto;
 import com.google.protobuf.util.JsonFormat;
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.*;
-
 
 public class Main {
     public static void main(String[] args) {
-        JsonFormat.Printer printer = JsonFormat.printer()
-                .includingDefaultValueFields()
-                .preservingProtoFieldNames();
-        for (List<String> row : rows()) {
-            ProductProto.Product product = ProductProto.Product.newBuilder()
-                    .setProductID(Integer.parseInt(row.get(0)))
-                    .setCatalog(row.get(1))
-                    .setStockcode(row.get(2))
-                    .setProductDescription(row.get(3)).build();
+        JsonFormat.Printer printer = JsonFormat.printer();
+        for (ProductProto.Product product : ProductGenerator.products()) {
             try {
                 String jsonProduct = printer.print(product);
                 System.out.println(jsonProduct);
@@ -25,21 +14,5 @@ public class Main {
                 e.printStackTrace();
             }
         }
-    }
-
-    public static List<List<String>> rows() {
-        List<List<String>> records = new ArrayList<>();
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader("ProductFile.csv"));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] values = line.split(",");
-                records.add(Arrays.asList(values));
-            }
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return records;
     }
 }
---
+++ src/main/java/org/example/ProductGenerator.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import com.sampleProject.Product.Proto.ProductProto;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ProductGenerator {
+    public static List<ProductProto.Product> products() {
+        List<ProductProto.Product> products = new ArrayList<>();
+        for (List<String> row : rows()) {
+            ProductProto.Product product = ProductProto.Product.newBuilder()
+                    .setProductID(Integer.parseInt(row.get(0)))
+                    .setCatalog(row.get(1))
+                    .setStockcode(row.get(2))
+                    .setProductDescription(row.get(3)).build();
+            products.add(product);
+        }
+        return products;
+    }
+
+    public static List<List<String>> rows() {
+        List<List<String>> records = new ArrayList<>();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("ProductFile.csv"));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] values = line.split(",");
+                records.add(Arrays.asList(values));
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return records;
+    }
+}
